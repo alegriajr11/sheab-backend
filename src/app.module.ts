07023a7048f0b6f37b4f1222aa54e95d69da5940
alegriajr11 <edwar.alegria@hotmail.com>
2023-11-29 12:48:24 -0500

    Agregando conexion a JawsDb

--- src/app.module.ts
+++ src/app.module.ts
@@ -177,11 +177,7 @@ import { BackupBdModule } from './backup_bd/backup_bd.module';
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         type: 'mysql',
-        host: configService.get<string>(DB_HOST),
-        port: +configService.get<number>(DB_PORT),
-        username: configService.get<string>(DB_USER),
-        password: configService.get<string>(DB_PASSWORD),
-        database: configService.get<string>(DB_DATABASE),
+        url: configService.get<string>('JAWSDB_URL'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: false,
         logging: false
