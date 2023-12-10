6690335ff2ba00d0f01400f4f3a522dfdc2e5bb4
alegriajr11 <edwar.alegria@hotmail.com>
2023-12-10 12:15:08 -0500

    Servicio y controlador divs creados

--- src/main.ts
+++ src/main.ts
@@ -13,10 +13,10 @@ async function bootstrap() {
   app.use(requestIp.mw());
 
   // Obtener el puerto de la variable de entorno proporcionada por Heroku
-  const port = process.env.PORT || 3000; // Usar 3000 si la variable de entorno no está definida
+  //const port = process.env.PORT || 3000; // Usar 3000 si la variable de entorno no está definida
 
   // Server Port loacalmente
-  //const port = +configService.get<number>(SERVER_PORT) || 3000;
+  const port = +configService.get<number>(SERVER_PORT) || 3000;
   await app.listen(port);
 }
 bootstrap();
